5139645ddaf9434701e338bdd051a77c4f602ed6
RoxanaLarisssa <roxana.larisssa@gmail.com>
2023-11-08 19:37:42 +0000

    update task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const color = {
+  startBtn: document.querySelector("button[data-start]"),
+  stopBtn: document.querySelector("button[data-stop]"),
+  body: document.querySelector("body"),
+};
 
+color.startBtn.addEventListener("click", startChangeColor);
+
+color.stopBtn.addEventListener("click", stopBtnChangeColor);
+
+let start = null;
+
+function startChangeColor() {
+  start = setInterval(bodyBcg, 1000);
+  color.startBtn.setAttribute("disabled", true);
+}
+
+function stopBtnChangeColor(e) {
+  clearInterval(start);
+  color.startBtn.removeAttribute("disabled");
+}
+
+function bodyBcg() {
+  color.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,6 @@
+import flatpickr from "flatpickr";
+// Import suplimentar de stil
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix'
+
+
